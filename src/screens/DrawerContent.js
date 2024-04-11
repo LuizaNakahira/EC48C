72384cf7ebed66cb06da1a5cf976a38b63b7f941
Luiza Xavier Nakahira <luizanakahira40@gmail.com>
2024-04-11 11:47:41 -0300

    finalizando estilização de drawer

--- src/screens/DrawerContent.js
+++ src/screens/DrawerContent.js
@@ -11,13 +11,13 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 const DrawerContent = (props) => {
     return(
         <DrawerContentScrollView {...props} contentContainerStyle={{ display: 'flex', flex: 1, justifyContent: 'space-between'}} style={{ backgroundColor: '#2B1F5C'}}>
-            <View  style={{ display: 'flex', flex: 1, justifyContent: 'space-evenly', maxHeight: 250}} >
+            <View  style={{ display: 'flex',  paddingVertical: 50,}} >
                
-                <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+                <View style={{ display: 'flex', flexDirection: 'row',paddingVertical: 20,  justifyContent: 'center'}}>
                     <Text style={{ color: 'white', fontSize: 25 }}>usuario@dominio.com</Text>
                 </View>
 
-                <View style={{ display: 'flex', alignItems: 'center' }}>
+                <View style={{ display: 'flex', alignItems: 'center', paddingVertical: 20,}}>
                     <Divider style={{ borderWidth: 1.1, borderColor: 'white', width: '80%' }} />
                 </View>
 
