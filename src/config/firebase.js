06c38f1939f2aa852ddad0f1a738b10e44749820
Luiza Xavier Nakahira <luizanakahira40@gmail.com>
2024-04-27 15:59:50 -0300

    firebase.js

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBz2mkjSBIONErDPNhjSb8X9riGruJJF6Q",
+    authDomain: "mydata-18dda.firebaseapp.com",
+    projectId: "mydata-18dda",
+    storageBucket: "mydata-18dda.appspot.com",
+    messagingSenderId: "917419768187",
+    appId: "1:917419768187:web:f00047091323d85bc825de"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app
